f17ed6ca9583e8605681985e0694aafd20e9a51b
QIQI <18612980825@163.com>
2021-11-18 11:00:18 +0100

    first commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for craft
+
+## Unreleased changes
---
+++ src/Chapter4.hs
@@ -0,0 +1,8 @@
+module MyModule (
+    maxThree
+    ) where
+
+maxThree :: Int ->Int ->Int->Int
+    maxThree = max (max x y) z
+
+    
